a876b8d66b4208b91791f415a7cae74976063fe9
Askar Yusupov <devex.soft@gmail.com>
2024-08-12 22:08:29 +0500

    feat(Engine.svelte): refactor script loading to use onMount for better lifecycle management feat(+page.svelte): integrate Engine component and pass cx prop for improved modularity and configuration

--- src/lib/components/Engine.svelte
+++ src/lib/components/Engine.svelte
@@ -1,30 +1,36 @@
 <script lang="ts">
 	import { init, createCallbacks } from '$lib/internal/store';
+	import { onMount } from 'svelte';
 
 	export let cx: string;
+	let src = 'https://cse.google.com/cse.js?cx=' + cx;
 
 	const scriptLoading = new Promise((resolve, reject) => {
-		window.__gcse = {
-			parsetags: 'explicit', // Defaults to 'onload'
-			initializationCallback() {
-				init.set(true);
-			},
-			searchCallbacks: {
-				image: createCallbacks('image'),
-				web: createCallbacks('web')
-			}
-		};
+		onMount(() => {
+			window.__gcse = {
+				parsetags: 'explicit', // Defaults to 'onload'
+				initializationCallback() {
+					init.set(true);
+				},
+				searchCallbacks: {
+					image: createCallbacks('image'),
+					web: createCallbacks('web')
+				}
+			};
 
-		const script = document.createElement('script');
-		script.src = 'https://cse.google.com/cse.js?cx=' + cx;
-
-		script.onload = resolve;
-		script.onerror = reject;
-
-		document.head.appendChild(script);
+			const script = document.createElement('script');
+			script.src = src;
+			script.onload = resolve;
+			script.onerror = reject;
+			document.head.appendChild(script);
+		});
 	});
 </script>
 
 {#await scriptLoading}
+	loading...
+{:then}
 	<slot />
+{:catch err}
+	error {err}
 {/await}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { PUBLIC_CSE_CX } from '$env/static/public';
+	import Engine from '$lib/components/Engine.svelte';
+
+	const cx = PUBLIC_CSE_CX;
+</script>
+
+<Engine {cx}>
+	<h1>Welcome to your library project</h1>
+	<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</Engine>

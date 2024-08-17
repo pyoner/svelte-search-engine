bb0de2b479b30991e1f2c73255834cba1ae99312
Askar Yusupov <devex.soft@gmail.com>
2024-08-17 16:15:44 +0500

    feat(lib/index.ts): add component and store exports to simplify imports and improve module usability

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,18 @@
 // Reexport your entry components here
+
+// export components
+export { default as Engine } from './components/Engine.svelte';
+export { default as Search } from './components/Search.svelte';
+export { default as SearchBox } from './components/SearchBox.svelte';
+export { default as SearchResults } from './components/SearchResults.svelte';
+
+// export stores
+export {
+	init as initStore,
+	ready as readyStore,
+	rendered as renderedStore,
+	starting as startingStore
+} from './stores';
+
+// export google api
+export { api as cseElementApi } from './internal/api';

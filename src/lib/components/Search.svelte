2ac2c635fd78d4cb0200e2f75118188f24764fa4
Askar Yusupov <devex.soft@gmail.com>
2024-08-17 12:40:54 +0500

    refactor(Search.svelte): update gname type from Gname to WithGname for better type alignment feat(search page): create a new search page that utilizes the Search component for web and image searches

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import type { Gname } from '$lib/types/base';
+	import type { WithGname } from '$lib/internal/types';
 	import type { UIComponents } from '$lib/types/components';
 	import type { ComponentAttributes } from '$lib/types/google';
 
 	import { gcseAction } from '$lib/internal/action';
 
-	export let attributes: ComponentAttributes & { gname: Gname };
+	export let attributes: ComponentAttributes & WithGname;
 	let { gname } = attributes;
 
 	export let components: UIComponents | undefined = undefined;
---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Results from '$lib/components/Results.svelte';
+	import Search from '$lib/components/Search.svelte';
+</script>
+
+<Search
+	attributes={{
+		gname: 'web',
+		enableImageSearch: false
+	}}
+	components={{ web: Results }}
+></Search>
+
+<Search
+	attributes={{
+		gname: 'image',
+		enableImageSearch: true,
+		disableWebSearch: true
+	}}
+></Search>

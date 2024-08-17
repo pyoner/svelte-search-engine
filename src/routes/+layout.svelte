34ee0a481e9e04277e7b6e2ab4f20ce5ab2902ad
Askar Yusupov <devex.soft@gmail.com>
2024-08-17 12:14:41 +0500

    feat(layout): create a new layout component to encapsulate the Engine component and improve code organization refactor(page): remove Engine component from page and restructure layout for better readability and maintainability

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { PUBLIC_CSE_CX } from '$env/static/public';
+	import Engine from '$lib/components/Engine.svelte';
+
+	const cx = PUBLIC_CSE_CX;
+</script>
+
+<Engine {cx}>
+	<slot></slot>
+</Engine>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,9 @@
 <script>
-	import { PUBLIC_CSE_CX } from '$env/static/public';
-	import Engine from '$lib/components/Engine.svelte';
 	import Results from '$lib/components/Results.svelte';
 	import Search from '$lib/components/Search.svelte';
 	import SearchBox from '$lib/components/SearchBox.svelte';
 	import SearchResults from '$lib/components/SearchResults.svelte';
 	import { rendered, ready } from '$lib/stores';
-
-	const cx = PUBLIC_CSE_CX;
 
 	ready.subscribe((input) => {
 		console.log('ready input', input);
@@ -18,36 +14,34 @@
 	});
 </script>
 
-<Engine {cx}>
-	<div class="container">
-		<div>
-			<Search
-				attributes={{
-					gname: 'web',
-					enableImageSearch: false
-				}}
-				components={{ web: Results }}
-			></Search>
-		</div>
-		<div>
-			<Search
-				attributes={{
-					gname: 'image',
-					enableImageSearch: true,
-					disableWebSearch: true
-				}}
-			></Search>
-		</div>
-		<div>
-			<SearchBox
-				attributes={{
-					gname: 'xxx'
-				}}
-			></SearchBox>
-			<SearchResults attributes={{ gname: 'xxx' }}></SearchResults>
-		</div>
+<div class="container">
+	<div>
+		<Search
+			attributes={{
+				gname: 'web',
+				enableImageSearch: false
+			}}
+			components={{ web: Results }}
+		></Search>
 	</div>
-</Engine>
+	<div>
+		<Search
+			attributes={{
+				gname: 'image',
+				enableImageSearch: true,
+				disableWebSearch: true
+			}}
+		></Search>
+	</div>
+	<div>
+		<SearchBox
+			attributes={{
+				gname: 'xxx'
+			}}
+		></SearchBox>
+		<SearchResults attributes={{ gname: 'xxx' }}></SearchResults>
+	</div>
+</div>
 
 <style>
 	.container {

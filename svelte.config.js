1e3897dc75317cccc17ee925260906469c85b8c4
Askar Yusupov <devex.soft@gmail.com>
2024-08-18 12:09:50 +0500

    chore: remove mdsvex dependency and related configuration to simplify project setup and reduce complexity

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex({ extension: '.md' })],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
